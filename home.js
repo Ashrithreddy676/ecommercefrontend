a349eafdeb04306e9058f46c51c8ac170ad0cdd0
= <=>
2025-08-01 10:27:35 +0530

    added

--- home.js
+++ home.js
@@ -1,7 +1,14 @@
 let home=()=>{
     return `
     <h1>Home</h1>
+    <a href="addProduct">Add Product</a>
     `
 }
 
+let anchor=document.querySelector("a")
+
+const handelClickAnchor=(e)=>{
+    e.preventDefault()
+    history.pushState({})
+}
 export default home
